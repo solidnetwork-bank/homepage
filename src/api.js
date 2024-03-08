650034ba958586fafefe0ae8ac58358139c9486d
Jose Osuna Barrios <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-08 17:39:33 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiStage = 'dev';
+const apiStage = {process.env.REACT_APP_API_GATEWAY_STAGE};
 
 export default axios.create({
   baseURL: `https://api.solidnetwork.xyz/${apiStage}/`
