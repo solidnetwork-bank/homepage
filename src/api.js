1852c2248c1ec76aa0a3dd89fc56ae35f14c8b50
Jose Osuna Barrios <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-08 22:56:09 +0000

    adds REACT_APP_API_GATEWAY_STAGE env to axios.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiStage = {process.env.REACT_APP_API_GATEWAY_STAGE};
+const apiStage = process.env.REACT_APP_API_GATEWAY_STAGE;
 
 export default axios.create({
   baseURL: `https://api.solidnetwork.xyz/${apiStage}/`
